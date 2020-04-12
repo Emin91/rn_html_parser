36e1a4cf1c1b59166cab197e63ec3673f380ff63
Emin91 <emin.proger@gmail.com>
2020-04-12 18:31:59 +0400

    Added full news screen

--- MainScreen.js
+++ MainScreen.js
@@ -3,13 +3,11 @@ import {
   View,
   Text,
   ScrollView,
-  Dimensions,
   Image,
   TouchableOpacity,
   Linking,
 } from 'react-native';
 import cheerio from 'react-native-cheerio';
-import HTML from 'react-native-render-html';
 
 const states = {
   initial: 'INITIAL',
@@ -20,32 +18,45 @@ const states = {
 
 const DOMAIN = 'http://www.xachmaz-ih.gov.az';
 
-const htmlContent = `
-    <h1>This HTML snippet is now rendered with native components !</h1>
-    <h2>Enjoy a webview-free and blazing fast application</h2>
-    <img src="https://i.imgur.com/dHLmxfO.jpg?2" />
-    <em style="textAlign: center;">Look at how happy this native cat is</em>
-`;
-
 const App = () => {
-  const [pageContent, setPageContent] = useState('');
+  const [news, setNews] = useState([]);
   const [status, setStatus] = useState(states.initial);
-  // console.log('pageContent', pageContent);
 
   useEffect(() => {
     const fetchNews = async () => {
       setStatus(states.pending);
       try {
-        const searchUrl = `http://www.xachmaz-ih.gov.az/news/364.html`;
+        const searchUrl = `http://www.xachmaz-ih.gov.az/news.html`;
         const response = await fetch(searchUrl);
         const htmlString = await response.text();
         const $ = cheerio.load(htmlString);
 
-        const html = $('.content_info')
-          .html()
-          .replace(/\/userfiles\/*/gi, `${DOMAIN}/userfiles/`);
+        const newsList = [];
 
-        setPageContent(html);
+        $('.news-list li').each((index, node) => {
+          const titleNode = $(node).find('.news-title a');
+          const title = titleNode.text();
+          const url = titleNode.attr('href');
+          const date = $(node)
+            .find('.news-date')
+            .text();
+          const description = $(node)
+            .find('.news-short-content')
+            .text();
+          const thumbnailURL = $(node)
+            .find('.news-thumb')
+            .attr('src');
+          const imgId = thumbnailURL.slice(thumbnailURL.lastIndexOf('/'));
+
+          newsList.push({
+            title,
+            date,
+            description,
+            imgId,
+            url,
+          });
+        });
+        setNews(newsList);
         setStatus(states.completed);
       } catch (e) {
         console.log('e', e);
@@ -58,12 +69,12 @@ const App = () => {
   if (status === states.pending || status === states.initial) {
     return (
       <View>
-        <Text>Loading...</Text>
+        <Text>Loading</Text>
       </View>
     );
   }
 
-  if (status === states.error) {
+  if (status === states.error || !news.length) {
     return (
       <View>
         <Text>Error</Text>
@@ -73,39 +84,12 @@ const App = () => {
 
   return (
     <View style={{flex: 1}}>
-      {/* <Image
+      <Image
         resizeMode="center"
         style={{width: '100%', height: '100%'}}
         source={{uri: DOMAIN + '/files/news' + news[0].imgId}}
         // source={{uri: DOMAIN + '/files/news/small' + news[0].imgId}}
-      /> */}
-      <ScrollView style={{flex: 1}}>
-        <HTML
-          html={pageContent}
-          renderers={{
-            // p: props => {
-            //   return <Text>jgjh</Text>;
-            // },
-            img: ({src}) => {
-              return (
-                <Image
-                  resizeMode="contain"
-                  source={{uri: src}}
-                  style={{width: '100%', height: 150}}
-                />
-              );
-            },
-          }}
-          // imagesMaxWidth={300}
-          // tagsStyles={{
-          //   img: {
-          //     width: 300,
-          //   },
-          // }}
-        />
-      </ScrollView>
-      <Text>sdsd</Text>
-
+      />
       {/* <ScrollView style={{flex: 1}}>
         {news.slice(0, 5).map(({title, thumbnailURL, url, date}) => {
           return (
